8327dd10a2d5c55eb028ea35825f822900499afa
zulfikar noer <zulfikar.khalid@bni.co.id>
2023-01-02 23:53:05 +0700

    adding user and pass validation

--- src/main/java/com/example/loginappdemo/entity/RequestModel.java
+++ src/main/java/com/example/loginappdemo/entity/RequestModel.java
@@ -1,11 +1,15 @@
 package com.example.loginappdemo.entity;
 
+import com.example.loginappdemo.validation.annotation.Matched;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class RequestModel {
+    @Matched(regex = "^[^%<>^$\\-]+$", message = "only alphabets and white spaces")
     protected String username;
+    @Matched(regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"
+            , message = "atleast 8 char long, one Capital, digits, and Special Character")
     protected String password;
 }
